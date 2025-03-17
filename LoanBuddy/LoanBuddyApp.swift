610a5192b1144d690d71e10f1b5430888372d1c4
SShrey1 <palishrey@gmail.com>
2025-03-17 14:03:03 +0530

    Initial Commit

---
+++ LoanBuddy/LoanBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanBuddyApp.swift
+//  LoanBuddy
+//
+//  Created by user@59 on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LoanBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
